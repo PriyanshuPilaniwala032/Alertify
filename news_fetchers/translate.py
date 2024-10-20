a5a52de68cb85c447446fd0029bf5d85d4ef0a9d
Alok Raj <140916651+Rajalok21@users.noreply.github.com>
2024-10-20 22:33:31 +0530

    Delete news_fetchers directory

--- news_fetchers/translate.py
+++
@@ -1,8 +0,0 @@
-from deep_translator import GoogleTranslator
-
-def translate_to_english(text):
-    try:
-        translator = GoogleTranslator(source='auto', target='en')
-        return translator.translate(text)
-    except Exception as e:
-        return f"Error: {str(e)}"
